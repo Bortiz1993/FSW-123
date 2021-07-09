5eb9e04216579cf849a4bc0558f0423e4de69a96
Brijido Ortiz Zuniga <77209112+Bortiz1993@users.noreply.github.com>
2021-07-09 15:47:50 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import TodoList from './TodoList';
+
+
+function App() {
+  return (
+    <div>
+ <h3>List of Todos</h3>
+ <TodoList/>
+  
+  </div>
+
+  
+  );
+}
+
+export default App;
---
+++ TodoList.js
@@ -0,0 +1,25 @@
+
+
+function TodoList() {
+
+    return (
+        <ul id="Main-List">
+            <li>
+            <input type="checkbox"/><span>go to Gym</span>
+            </li>
+            <li>
+                <input type="checkbox"/><span>do laundry</span>
+
+            </li>
+            <li>
+                <input type="checkbox"/><span>Food shopping</span>
+
+            </li>
+            <li>
+                <input type="checkbox"/><span>Do homework</span>
+            </li>
+        </ul>
+    );
+}
+
+export default TodoList;
